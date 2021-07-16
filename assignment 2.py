b4dd87b9ec58afceff75a7664bce75081fb67b54
Sukanyacse <68785445+Sukanyacse@users.noreply.github.com>
2021-07-16 21:38:11 +0530

    Add files via upload

--- assignment 2.py
+++ assignment 2.py
@@ -6,7 +6,7 @@ for value in range(1,10):
 		count_even=count_even+1
 	else:
 		count_odd=count_odd+1
-print("Number of even numbers",count_even)
-print("Number of odd numbers",count_odd)
+print("Number of even numbers:",count_even)
+print("Number of odd numbers:",count_odd)
 
 
